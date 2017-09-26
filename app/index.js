99159c12394eba05120cb1b2f847660f4e1459bb
Yasmina Gavaldà <y.gavalda@gmail.com>
2017-09-26 20:56:58 +0200

    App & Popular components

--- app/index.js
+++ app/index.js
@@ -1,20 +1,7 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 require('./index.css')
-
-// state
-// lifecycle event
-// UI 
-
-class App extends React.Component {
-	render() {
-    return (
-      <div>
-        Hello world!
-      </div>
-    )
-  }
-}
+var App = require('./components/App')
 
 ReactDOM.render(
   <App />,
