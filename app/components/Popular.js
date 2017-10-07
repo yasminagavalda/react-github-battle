f3b5f12d55613a5ffd919f8ed061792037cbe9e2
Yasmina Gavaldà <y.gavalda@gmail.com>
2017-10-08 01:37:24 +0200

    Loading component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 var React = require('react')
 var PropTypes = require('prop-types')
 var api = require('../utils/api')
+var Loading = require('./Loading')
 
 function SelectLanguage (props) {
 	var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
@@ -96,7 +97,7 @@ class Popular extends React.Component {
 	      	onSelect = {this.updateLanguage}
 	      />
         {!this.state.repos
-          ? <p>LOADING</p>
+          ? <Loading speed={100}/>
           : <RepoGrid repos={this.state.repos} />}
 	    </div>
     )
