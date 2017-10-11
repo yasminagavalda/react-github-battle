0a6fa8d07130af99032423180aac0f5f36ca8247
Yasmina Gavaldà <y.gavalda@gmail.com>
2017-10-11 15:54:12 +0200

    Refactoring async/await functions

--- app/components/Results.js
+++ app/components/Results.js
@@ -48,26 +48,27 @@ class Results extends React.Component {
 		loading: true
 	}
 
-	componentDidMount() {
+	async componentDidMount() {
 		const { playerOneName, playerTwoName } = queryString.parse(this.props.location.search)
-		battle([
+		
+		const players = await battle([
 			playerOneName,
 			playerTwoName
-		]).then((results) => {
-			if (results === null) {
-				return this.setState(() => ({
-					error: 'Looks like there was error. Check that both users exist on GitHub', 
-					loading: false
-				}))
-			}
+		])
 
-			this.setState(() => ({
-				error: null,
-				winner: results[0],
-				loser: results[1],
+		if (results === null) {
+			return this.setState(() => ({
+				error: 'Looks like there was error. Check that both users exist on GitHub', 
 				loading: false
 			}))
-		})
+		}
+
+		this.setState(() => ({
+			error: null,
+			winner: results[0],
+			loser: results[1],
+			loading: false
+		}))
 	}
   render() {
   	const { error, winner, loser, loading } = this.state
