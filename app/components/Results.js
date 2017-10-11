78a4ba1ef61e7b2db9f8c74c1b530da1e6d66da5
Yasmina Gavaldà <y.gavalda@gmail.com>
2017-10-11 15:15:50 +0200

    Refactoring import, export modules

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,10 +1,10 @@
-const React = require('react')
-const PropTypes = require('prop-types')
-const queryString = require('query-string')
-const api = require('../utils/api')
-const Link = require('react-router-dom').Link
-const PlayerPreview = require('./PlayerPreview')
-const Loading = require('./Loading')
+import React from 'react'
+import PropTypes from 'prop-types'
+import queryString from 'query-string'
+import { battle } from '../utils/api'
+import { Link } from 'react-router-dom'
+import PlayerPreview from './PlayerPreview'
+import Loading from './Loading'
 
 function Profile ({ info }) {
 	const { avatar_url, login, name, location, company, followers, following, public_repos, blog } = info
@@ -53,7 +53,7 @@ class Results extends React.Component {
 	}
 	componentDidMount() {
 		const { playerOneName, playerTwoName } = queryString.parse(this.props.location.search)
-		api.battle([
+		battle([
 			playerOneName,
 			playerTwoName
 		]).then((results) => {
@@ -105,4 +105,4 @@ class Results extends React.Component {
   }
 }
 
-module.exports = Results
+export default Results
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,4 +1,4 @@
-const axios = require('axios')
+import axios from 'axios'
 
 const id = "YOUR_CLIENT_ID"
 const sec = "YOUR_SECRET_ID"
@@ -40,17 +40,15 @@ function sortPlayers (players) {
 	return players.sort((a,b) => b.score - a.score)
 }
 
-module.exports = {
-  battle (players) {
-  	return Promise.all(players.map(getUserData))
-  		.then(sortPlayers)
-  		.catch(handleError)
-  },
-  fetchPopularRepos (language) {
-    const encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
-
-    return axios.get(encodedURI)
-      .then(({ data }) => data.items)
-  }
+export function battle (players) {
+  return Promise.all(players.map(getUserData))
+    .then(sortPlayers)
+    .catch(handleError)
 }
 
+export function fetchPopularRepos (language) {
+  const encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
+
+  return axios.get(encodedURI)
+      .then(({ data }) => data.items)
+}
